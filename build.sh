07059c87863b7fc7f2079d3e2dd9868e8eda9621
brakobby <brakobby454@gmail.com>
2025-11-21 01:19:02 +0000

    included pandas in requirements.txt 222

--- build.sh
+++ build.sh
@@ -2,10 +2,14 @@
 # Exit on error
 set -o errexit
 
-# Create and activate virtual environment (Render does this automatically)
+# Upgrade pip
+pip install --upgrade pip
 
 # Install dependencies
 pip install -r requirements.txt
+
+# Create static directory if it doesn't exist
+mkdir -p static
 
 # Collect static files
 python manage.py collectstatic --noinput
